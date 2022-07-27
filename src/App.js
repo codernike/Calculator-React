ad72906f798e088de05a58b71727e03d4d023dc6
codernike <108070610+codernike@users.noreply.github.com>
2022-07-27 22:21:06 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import Calc from './lib/calc'
-import Footer from './lib/footer'
-import Navbar from './lib/navbar'
+
 
 export default function App() {
   return (
   <>
-  
-  <Navbar/>
-  <div ></div>
-  <div class="container">
+ 
  <Calc/>
-  </div>
-  <Footer/>
+ 
   </>
   )
 }
--- src/lib/calc.jsx
+++ src/lib/calc.jsx
@@ -1,54 +1,105 @@
-import React, { useState } from 'react'
-import './calc.css'
-const Calc = props => {
-    const [ result, setResult] = useState(0);
-    function handelclick(e){
-     let val = e.target.value;
-     setResult(val+=result)
+import {  useState } from 'react'
+//import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [display, setDisplay] = useState('0')
+  const [incomma, setIncomma] = useState(false)
+
+  const addNumber = (e) => {
+    const btnKey = e.target.innerText
+    if(display==='0'){
+      setDisplay(btnKey)
+    }else{
+      setDisplay(display+btnKey)
     }
-    
+  }
+
+  const addOperator = (e) => {
+    let oprKey = e.target.innerText
+    if(oprKey==='x'){
+      oprKey = '*'
+    }
+    let oprCount = 0
+    let revStr = display.split('').reverse()
+    for(let symbol of revStr){
+      if(symbol==='-' || symbol==='+' || symbol==='*' || symbol==='/'){
+        oprCount+=1
+      }else{
+        break
+      }
+    }
+    if(oprCount===0){
+      setDisplay(display+oprKey)
+    }else{
+      setDisplay(display.slice(0,-oprCount)+oprKey)
+    }
+    setIncomma(false)
+  }
+
+  const subtract = () => {
+    setDisplay(display+'-')
+    setIncomma(false)
+  }
+
+  const addDecimal = () => {
+    if(incomma===false){
+      setDisplay(display+'.')
+      setIncomma(true)
+    }else{
+      setDisplay(display)
+    }
+  }
+
+  const clearDisplay = () => {
+    setDisplay('0')
+    setIncomma(false)
+  }
+
+  const calculate = () => {
+    setDisplay(eval(display)+'')
+    if(display.includes('.',0)){
+      setIncomma(true)
+    }else{
+      setIncomma(false)
+    }
+  }
+
   return (
-    <>
-    <center>
-         <form name="calc" >
-        <table>
-            <tr>
-                <td>
-                    <input type="text" value={result} size="16" id="answer"/>
-    
-                    <br/>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input  onclick={()=>result+=1} type="button" name="2" value="  1  " />
-                    <input onclick={handelclick} type="button" name="2" value="  2  " />
-                    <input onclick={handelclick} type="button" name="3" value="  3  " />
-                    <input onclick={handelclick} type="button" class="+" name="plus" value="  +  "/>
-                    <br/>
-                    <input onclick={handelclick} type="button" name="4" value="  4  " />
-                    <input onclick={handelclick} type="button" name="5" value="  5  " />
-                    <input onclick={handelclick} type="button" name="6" value="  6  " />
-                    <input onclick={handelclick} type="button" class="-" name="-" value="  -  " />
-                    <br/>
-                    <input onclick={handelclick} type="button" name="7" value="  7  " />
-                    <input onclick={handelclick} type="button" name="8" value="  8  " />
-                    <input onclick={handelclick} type="button" name="9" value="  9  " />
-                    <input onclick={handelclick} type="button" class="*" name="times" value="  x  " />
-                    <br/>
-                    <input onclick={handelclick} type="button" id="clear" name="clear" value="  c  " />
-                    <input onclick={handelclick} type="button" name="0" value="  0  " />
-                    <input onclick={handelclick} type="button" name="doit" value="  =  " />
-                    <input onclick={handelclick} type="button" class="/" name="div" value="  /  " />
-                    <br/>
-                </td>
-            </tr>
-        </table>
-    </form>
-    </center> 
-    </>
+    <div className="container">
+      <div className="desc">
+        <h1>React Calculator</h1>
+        <p>Copyright 2022 by Calculator.com</p>
+      </div>
+      <div className="calculator">
+        <div id="display">{display}</div>
+        <div className="buttons">
+          <button id="clear" className="button" onClick={clearDisplay}>clear</button>
+          <a href="/" className="repo" target="_blank" rel="noopener noreferrer"><i className="bi bi-github"></i></a>
+          
+          <button id="seven" className="button" onClick={addNumber}>7</button>
+          <button id="eight" className="button" onClick={addNumber}>8</button>
+          <button id="nine" className="button" onClick={addNumber}>9</button>
+          <button id="add" className="button" onClick={addOperator}>+</button>
+
+          <button id="four" className="button" onClick={addNumber}>4</button>
+          <button id="five" className="button" onClick={addNumber}>5</button>
+          <button id="six" className="button" onClick={addNumber}>6</button>
+          <button id="subtract" className="button" onClick={subtract}>-</button>
+
+          <button id="one" className="button" onClick={addNumber}>1</button>
+          <button id="two" className="button" onClick={addNumber}>2</button>
+          <button id="three" className="button" onClick={addNumber}>3</button>
+          <button id="multiply" className="button" onClick={addOperator}>x</button>
+
+          <button id="decimal" className="button" onClick={addDecimal}>.</button>
+          <button id="zero" className="button" onClick={addNumber}>0</button>
+          <button id="equals" className="button" onClick={calculate}>=</button>
+          <button id="divide" className="button" onClick={addOperator}>/</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
-
-export default Calc;
+export default App
